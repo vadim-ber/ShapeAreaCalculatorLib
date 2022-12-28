ef21b562cfd0acd1e0a324b3a4eb3d9164f72277
vadim-ber <grondus@gmail.com>
2022-12-28 12:45:52 +0200

    Добавьте файлы проекта.

---
+++ ShapeCreator.cs
@@ -0,0 +1,45 @@
+﻿using System.Data;
+using System.Reflection;
+
+namespace ShapeAreaCalculator
+{
+
+    public delegate void ShapeCheatorHandler(string message);
+    public static class ShapeCreator
+    {
+
+        public static void GetShapeArea(string shapetypename, double[] shapeparams, ShapeCheatorHandler del)
+        {
+            string typeName = (Char.ToUpper(shapetypename[0]) + shapetypename[1..].ToLowerInvariant());
+            var type = Type.GetType("ShapeAreaCalculator." + typeName);
+            if (type != null)
+            {
+                try
+                {
+                    var shape = (Shape)Activator.CreateInstance(type)!;
+                    del(shape.CalcArea(shapeparams));
+                }
+                catch (Exception ex)
+                {
+                    del(ex.Message);
+                }
+            }
+            else { del("This type of figure is not found."); }
+
+        }
+
+        public static void GetRelisedShapeTypes(ShapeCheatorHandler del)
+        {
+            // Динамический список фигур, площадь которых доступна для вычисления.
+
+            Type t = typeof(Shape);
+
+            IEnumerable<Type> list = Assembly.GetAssembly(t)!.GetTypes().Where(type => type.IsSubclassOf(t));
+
+            foreach (Type itm in list)
+            {
+                del(itm.Name + " ");
+            }
+        }
+    }
+}
---
+++ ShapeTypes.cs
@@ -0,0 +1,66 @@
+﻿namespace ShapeAreaCalculator
+{
+
+    public abstract class Shape
+    {
+
+        internal abstract string CalcArea(double[] data);
+
+    }
+
+    public class Circle : Shape
+    {
+        internal override string CalcArea(double[] data)
+        {
+            if (data.Length == 1)
+            {
+                double radius = data[0];
+                double area = Math.PI * radius * radius;
+                return "Circle with area: " + area.ToString();
+            }
+            else { return "Wrong input."; }
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        internal override string CalcArea(double[] data)
+        {
+            if (data.Length == 3)
+            {
+                double sideA = data[0];
+                double sideB = data[1];
+                double sideC = data[2];
+
+                if ((sideA + sideB) > sideC & (sideA + sideC) > sideB & (sideB + sideC) > sideA)
+                {
+
+                    double p = (sideA + sideB + sideC) / 2;
+
+                    bool rec = IsRectangular(sideA, sideB, sideC);
+
+                    double area = (Math.Sqrt(p * (p - sideA) * (p - sideB) * (p - sideC)));
+
+                    return "Triangle with area: " + area.ToString() + ". Is rectangular: " + rec + ".";
+                }
+                else { return "Triangle does not exist."; }
+
+            }
+            else { return "Wrong input."; }
+        }
+        private static bool IsRectangular(double sideA, double sideB, double sideC)
+        {
+            double[] arrSides = { sideA, sideB, sideC };
+            Array.Sort(arrSides);
+
+            if (Math.Pow(arrSides[2], 2) - Math.Pow(arrSides[1], 2) - Math.Pow(arrSides[0], 2) == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
